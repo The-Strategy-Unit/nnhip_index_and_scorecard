350e0c7cb99e3cbad7ad784b78c83f7f54790629
Craig Parylo <105867449+craig-parylo@users.noreply.github.com>
2026-01-13 18:29:12 +0000

    :hammer: adds a utility.R script to hold small, utility-focussed functions

---
+++ R/utils.R
@@ -0,0 +1,14 @@
+# Helper functions ------------------------------------------------------------
+# These are short, utility functions to help facilitate tasks.
+
+#' Render the readme file
+#'
+#' @description
+#' Helper script to render the README.md from the README.Rmd file.
+#'
+#' @returns NULL
+#'
+#' @noRd
+render_readme <- function() {
+  rmarkdown::render("README.Rmd", output_format = "md_document")
+}
