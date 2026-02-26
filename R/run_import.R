b1539bcd88d778c543c14318ba3b126b59fb83c6
Andre-StrategyUnit <andre.bilbrough@mlcsu.nhs.uk>
2026-02-26 12:08:54 +0000

    Update run_import.R

--- R/run_import.R
+++ R/run_import.R
@@ -16,13 +16,19 @@ source("functions.R")
 # define file location
 file_location = "tests/input/valid"
 
-# run each function in order
+# check validity of submission basics
 Check_Files1a(path_name = file_location)
 Check_Files1b(path_name = file_location)
 Check_Files2(path_name = file_location)
 Check_Files3(path_name = file_location)
 Check_Files4(path_name = file_location)
+
+# import data
+# to import data with invalid values for a variable, change the argument to c(TRUE, FALSE)
+# i.e. to import files with invalid metrics, acceptable_valid_metrics = c(TRUE, FALSE)
 Import_Data(path_name = file_location)
+
+# check for NAs and missing data
 Check_Data1()
 
 # 3 alternatively the process can be run all at once ----
