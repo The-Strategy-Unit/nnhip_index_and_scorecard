a97975523143f969ee8421945660b46a09ce6845
Craig Parylo <105867449+craig-parylo@users.noreply.github.com>
2026-03-27 10:01:54 +0000

    :pencil2: fix a typo

--- R/01_prepare_app_data.R
+++ R/01_prepare_app_data.R
@@ -3,7 +3,7 @@
 #'
 #' This code is designed to be run each month to:
 #' 1. identify and load monthly Excel submissions from
-#'    Neighbhourhood Places
+#'    Neighbourhood Places
 #' 2. process this data to a tidy long format
 #' 3. update the data on the SU's Posit Connect server so it is available for
 #'    the app to use
