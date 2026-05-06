bcddf40783b1e1c40ff4c3c6aaa4422af885be0c
Craig Parylo <105867449+craig-parylo@users.noreply.github.com>
2026-05-06 20:39:04 +0100

    :bug: makes the `output$pin_last_updated()` object more reactive by including a `shiny::invalidateLater()` with a tapered timer, that refreshes the UI value more frequently when the data is new, i.e. to show minute-by-minute aging of the data when it is young, but falls back to hourly updates when the data is over a day old. The current configuration is: - refresh every 30 seconds when the data is less than an hour old - refresh every minute when the data is less than six hours old - refresh every five minutes when the data is less than 24 hours old - otherwise refresh hourly

--- outputs/reporting_app/R/mod_utils_last_updated.R
+++ outputs/reporting_app/R/mod_utils_last_updated.R
@@ -15,7 +15,34 @@ mod_utils_last_updated_ui <- function(id) {
 # server ----
 mod_utils_last_updated_server <- function(id, time) {
   shiny::moduleServer(id, function(input, output, session) {
+    # reactive that determines how often to update the UI
+    update_interval <- shiny::reactive({
+      shiny::req(time())
+      t <- time()
+      if (inherits(t, "Date")) {
+        t <- as.POSIXct(t)
+      }
+
+      # how old is the time data?
+      age <- difftime(time1 = Sys.time(), time2 = t, units = "secs")
+
+      # decide how often to update the UI
+      if (age < 3600) {
+        1e3 * 10 # update every 10 seconds (< 1 hour old)
+      } else if (age < 3600 * 6) {
+        1e3 * 60 # update every minute (< 6 hours old)
+      } else if (age < 3600 * 24) {
+        1e3 * 60 * 5 # update every 5 minutes (< 24 hours old)
+      } else {
+        1e3 * 60 * 60 # update every hour (>= 1 day old)
+      }
+    })
+
+    # update the age of the data
     output$pin_last_updated <- shiny::renderUI({
+      # force re-execution based on adaptive interval
+      shiny::invalidateLater(millis = update_interval(), session = session)
+
       req(time())
       t <- time()
       if (inherits(t, "Date")) {
